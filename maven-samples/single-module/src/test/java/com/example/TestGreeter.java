d8a989f0380b74153e10a24c9e43c11a3addb9fd
onesaif <136352437+onesaif@users.noreply.github.com>
2025-02-07 16:05:00 +0530

    removing the extra test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -27,11 +27,6 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
   @Test
-  public void identifyThisTest(){
-    assertTrue(true);
-  }
-
-  @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
 
